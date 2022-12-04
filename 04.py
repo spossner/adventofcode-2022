3f4efee95b62b1cecdc71151a254cc0749e19fa1
spossner <github@possner.de>
2022-12-04 17:20:50 +0100

    day 4 - some range overlap stuff...

---
+++ 04.py
@@ -0,0 +1,112 @@
+import os
+import sys
+import math
+import operator
+import re
+from collections import deque, defaultdict, namedtuple, Counter
+from functools import total_ordering, reduce
+from itertools import permutations, zip_longest, count
+from os.path import exists
+import bisect
+
+import requests
+import networkx as nx
+import numpy as np
+from aoc import Point, Rect, NORTH, rot_ccw, rot_cw, translate, get_ints
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+class Solution:
+    def __init__(self, data, modified=False, dev=False, do_strip=False, do_splitlines=True, split_char=None):
+        if data and do_strip and type(data) == str:
+            data = data.strip()
+        if data and do_splitlines and type(data) == str:
+            data = data.splitlines()
+        if data and split_char is not None:
+            if split_char == '':
+                data = [list(row) for row in data] if do_splitlines else list(data)
+            else:
+                data = [row.split(split_char) for row in data] if do_splitlines else data.split(split_char)
+        self.data = data
+        self.modified = modified
+        self.dev = dev
+
+    def contains(self, r, subr):
+        if not subr:
+            return True
+        if not r:
+            return False
+        if len(subr) > 1 and subr.step % r.step:
+            return False
+        return subr[0] in r and subr[-1] in r
+
+    def overlaps(self, r1, r2):
+        if not r1 or not r2:
+            return False
+
+        # ...#######....
+        # .....#######
+        if r1[0] <= r2[0] and r2[0] <= r1[-1]:
+            return True
+
+        # .....#######
+        # ...#######....
+        if r2[0] <= r1[0] and r1[0] <= r2[-1]:
+            return True
+
+        return False
+
+    def range_from_pair(self, pair_as_string):
+        bound = list(map(int, pair_as_string.split("-")))
+        return range(bound[0], bound[1] + 1)
+
+    def first_part(self):
+        total = 0
+        for pair in self.data:
+            r1, r2 = map(self.range_from_pair, pair)
+            if self.contains(r1, r2) or self.contains(r2, r1):
+                total += 1
+
+        return total
+
+    def second_part(self):
+        total = 0
+        for pair in self.data:
+            r1, r2 = map(self.range_from_pair, pair)
+            if self.overlaps(r1, r2):
+                total += 1
+        return total
+
+        return self.first_part()
+
+
+if __name__ == '__main__':
+    script = os.path.splitext(os.path.basename(sys.argv[0]))[0]
+    if '-' in script:
+        script = script.split('-')[0]
+
+    DEV = False
+    PART2 = True
+
+    STRIP = True
+    SPLIT_LINES = True
+    SPLIT_CHAR = ","
+    DATA = None
+    AOC_SESSION = os.environ.get('AOC_SESSION')
+
+    DATA_URL = f"https://adventofcode.com/2022/day/{int(script)}/input"
+
+    if not DATA:
+        file_name = f"{script}-dev{DEV if type(DEV) != bool else ''}.txt" if DEV else f"{script}.txt"
+        if exists(file_name):
+            with open(file_name) as f:
+                DATA = f.read()
+        elif AOC_SESSION and DATA_URL:
+            DATA = requests.get(DATA_URL, headers={'Cookie': f"session={AOC_SESSION}"}).text
+            with open(file_name, "w") as f:
+                f.write(DATA)
+
+    s = Solution(DATA, PART2, DEV, STRIP, SPLIT_LINES, SPLIT_CHAR)
+    print(s.first_part() if not PART2 else s.second_part())
